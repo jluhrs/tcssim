5bee0c1641af0e91b448a04e4b4ab2c2884f86e1
Javier Lührs <jluhrs@gemini.edu>
2023-10-03 12:21:13 -0300

    Added rotator commands.

---
+++ modules/tcssim/src/main/scala/tcssim/RotatorCmds.scala
@@ -0,0 +1,31 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package tcssim
+
+import cats.effect.Resource
+import tcssim.epics.EpicsServer
+
+trait RotatorCmds[F[_]] {
+  val park: CadRecord[F]
+}
+
+object RotatorCmds {
+
+  private val rotatorParkSuffix: String = "rotPark"
+  private val rotatorStopSuffix: String = "rotStop"
+  private val rotatorMove: String       = "rotMove"
+
+  case class RotatorCmdsImpl[F[_]] private (
+    park: CadRecord[F],
+    stop: CadRecord2[F],
+    move: CadRecord1[F]
+  ) extends RotatorCmds[F]
+
+  def build[F[_]](server: EpicsServer[F], top: String): Resource[F, RotatorCmds[F]] = for {
+    park <- CadRecord.build(server, top + rotatorParkSuffix)
+    stop <- CadRecord2.build(server, top + rotatorStopSuffix)
+    move <- CadRecord1.build(server, top + rotatorMove)
+  } yield RotatorCmdsImpl(park, stop, move)
+
+}
